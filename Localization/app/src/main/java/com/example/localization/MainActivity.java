a99cd6974ea39cb1f02bcf14b6b4f2b963da5e6f
applejian <478356182@qq.com>
2021-10-05 11:27:07 +0800

    add samples

---
+++ Localization/app/src/main/java/com/example/localization/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.localization;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView, textView2;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        textView2 = findViewById(R.id.textView2);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView2.setText(R.string.textview2);
+            }
+        });
+    }
+}
