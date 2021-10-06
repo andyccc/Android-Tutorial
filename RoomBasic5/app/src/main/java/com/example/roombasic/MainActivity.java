6951921f45ac6e587dea1cf8402de928bc189ced
applejian <478356182@qq.com>
2021-10-06 18:21:39 +0800

    add samples

---
+++ RoomBasic5/app/src/main/java/com/example/roombasic/MainActivity.java
@@ -0,0 +1,142 @@
+package com.example.roombasic;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button insertBtn, updateBtn, clearBtn, deleteBtn;
+    RecyclerView recyclerView;
+    Switch aSwitch;
+
+    WordViewModel wordViewModel;
+    Word lastWord;
+    MyAdapter myAdapter1, myAdapter2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        wordViewModel = new ViewModelProvider(this, new ViewModelProvider.AndroidViewModelFactory(getApplication())).get(WordViewModel.class);
+
+        myAdapter1 = new MyAdapter(false, wordViewModel);
+        myAdapter2 = new MyAdapter(true, wordViewModel);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(myAdapter1);
+
+        insertBtn = findViewById(R.id.buttonInsert);
+        updateBtn = findViewById(R.id.buttonUpdate);
+        clearBtn = findViewById(R.id.buttonClear);
+        deleteBtn = findViewById(R.id.buttonDelete);
+        aSwitch = findViewById(R.id.switchBtn);
+
+
+        wordViewModel.getAllWordsLive().observe(this, new Observer<List<Word>>() {
+            @Override
+            public void onChanged(List<Word> words) {
+                int temp = myAdapter1.getItemCount();
+
+                myAdapter1.setAllWords(words);
+                myAdapter2.setAllWords(words);
+
+                if (temp!= words.size()) {
+                    myAdapter1.notifyDataSetChanged();
+                    myAdapter2.notifyDataSetChanged();
+                }
+
+            }
+        });
+
+        aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked ) {
+                    recyclerView.setAdapter(myAdapter2);
+                } else {
+                    recyclerView.setAdapter(myAdapter1);
+                }
+            }
+        });
+
+        insertBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String[] english = {
+                        "hello",
+                        "world",
+                        "android",
+                        "google",
+                        "project",
+                        "view",
+                        "data"
+                };
+                String[] chinese = {
+                        "你好",
+                        "世界",
+                        "安卓",
+                        "谷歌",
+                        "项目",
+                        "视图",
+                        "数据"
+                };
+
+                for (int i = 0; i < english.length; i++) {
+                    Word w = new Word(english[i], chinese[i]);
+                    wordViewModel.insertWords(w);
+                }
+            }
+        });
+
+        updateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Word w = lastWord;
+                // 修改最后一条记录
+                if (null != w) {
+                    w.setWord("Hi");
+                    w.setChineseMeaning("你好啊");
+                } else {
+                    w = new Word("Hi", "你好啊");
+                    w.setId(25);
+                }
+
+                wordViewModel.updateWords(w);
+            }
+        });
+
+        clearBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                wordViewModel.deleteAllWords();
+            }
+        });
+
+        deleteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Word w = lastWord;
+                // 删除最后一条记录
+                if (null != w) {
+                    wordViewModel.deleteWords(w);
+                }
+            }
+        });
+
+    }
+
+}
---
+++ RoomBasic5/app/src/main/java/com/example/roombasic/WordDatabase.java
@@ -0,0 +1,62 @@
+package com.example.roombasic;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.room.Database;
+import androidx.room.DatabaseConfiguration;
+import androidx.room.InvalidationTracker;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.room.migration.Migration;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+import androidx.sqlite.db.SupportSQLiteOpenHelper;
+
+@Database(entities = {Word.class}, version = 5, exportSchema = false)
+public abstract class WordDatabase extends RoomDatabase {
+
+    private static WordDatabase INSTANCE;
+    static synchronized WordDatabase getDatabase(Context context) {
+        if (null == INSTANCE) {
+            INSTANCE = Room.databaseBuilder(context.getApplicationContext(), WordDatabase.class, "wordDatabase")
+//                    .fallbackToDestructiveMigration() // 破坏式 清空所有数据，更新表结构
+//                    .allowMainThreadQueries() // 允许在主线程执行
+                    .addMigrations(MIGRATION_4_5)
+                    .build();
+
+        }
+        return INSTANCE;
+    }
+
+    public abstract WordDao getWordDao();
+
+    // 字段增加
+    static final Migration MIGRATION_2_3 = new Migration(2,3) {
+        @Override
+        public void migrate(@NonNull SupportSQLiteDatabase database) {
+            database.execSQL("ALTER TABLE WORD ADD COLUMN BAR_DATA INTEGER NOT NULL DEFAULT 1");
+            database.execSQL("ALTER TABLE WORD ADD COLUMN FOO_DATA INTEGER NOT NULL DEFAULT 1");
+        }
+    };
+
+    // 字段减少
+    static final Migration MIGRATION_3_4 = new Migration(3,4) {
+        @Override
+        public void migrate(@NonNull SupportSQLiteDatabase database) {
+            database.execSQL("CREATE TABLE WORD_TEMP (id INTEGER PRIMARY KEY NOT NULL, english_word TEXT, chinese_meaning TEXT)");
+            database.execSQL("INSERT INTO WORD_TEMP (id, english_word, chinese_meaning)" + "" +
+                    "SELECT id, english_word, chinese_meaning FROM WORD");
+            database.execSQL("DROP TABLE WORD");
+            database.execSQL("ALTER TABLE WORD_TEMP RENAME TO WORD");
+        }
+    };
+
+    // 字段减少
+    static final Migration MIGRATION_4_5 = new Migration(4,5) {
+        @Override
+        public void migrate(@NonNull SupportSQLiteDatabase database) {
+            database.execSQL("ALTER TABLE WORD ADD COLUMN chinese_invisible INTEGER NOT NULL DEFAULT 1");
+        }
+    };
+
+}
