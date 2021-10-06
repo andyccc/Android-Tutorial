b52756217d6fc44488d4859f25f031fcd80cdc44
applejian <478356182@qq.com>
2021-10-06 22:15:51 +0800

    add samples

---
+++ WordsStep2/app/src/main/java/com/example/words/AddFragment.java
@@ -0,0 +1,141 @@
+package com.example.words;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.SavedStateViewModelFactory;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link AddFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class AddFragment extends Fragment {
+
+    private EditText editTextEnglish, editTextChinese;
+    private Button buttonSubmit;
+
+    private WordViewModel wordViewModel;
+
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public AddFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment AddFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static AddFragment newInstance(String param1, String param2) {
+        AddFragment fragment = new AddFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        wordViewModel = new ViewModelProvider(requireActivity(), new SavedStateViewModelFactory(requireActivity().getApplication(), requireActivity())).get(WordViewModel.class);
+
+        editTextEnglish = requireActivity().findViewById(R.id.editTextEnglish);
+        editTextChinese = requireActivity().findViewById(R.id.editTextChinese);
+        buttonSubmit = requireActivity().findViewById(R.id.buttonSubmit);
+        buttonSubmit.setEnabled(false);
+
+        // 获取焦点
+        editTextEnglish.requestFocus();
+
+        // 弹出键盘
+        InputMethodManager imm= (InputMethodManager)requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.showSoftInput(editTextEnglish, 0);
+
+        // 监听有内容就使得按钮可点击
+        TextWatcher textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String english = editTextEnglish.getText().toString().trim();
+                String chinese = editTextChinese.getText().toString().trim();
+                buttonSubmit.setEnabled(!english.isEmpty() && !chinese.isEmpty());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        };
+
+        editTextEnglish.addTextChangedListener(textWatcher);
+        editTextChinese.addTextChangedListener(textWatcher);
+
+        buttonSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String english = editTextEnglish.getText().toString().trim();
+                String chinese = editTextChinese.getText().toString().trim();
+                Word word = new Word(english, chinese);
+                wordViewModel.insertWords(word);
+
+                // 返回
+                NavController navController = Navigation.findNavController(v);
+                navController.navigateUp();
+
+                // 隐藏键盘
+                imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+            }
+        });
+
+    }
+}
