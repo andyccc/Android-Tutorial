88e2090f4209b8d5531bce3391ae5f3b468ecbf2
applejian <478356182@qq.com>
2021-10-31 16:39:23 +0800

    video player finish

--- MyPlayer/app/src/main/java/com/example/myplayer/MainActivity.kt
+++ MyPlayer/app/src/main/java/com/example/myplayer/MainActivity.kt
@@ -7,10 +7,15 @@ import android.view.Gravity
 import android.view.SurfaceHolder
 import android.view.View
 import android.widget.FrameLayout
+import android.widget.SeekBar
 import android.widget.VideoView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.controller_layout.*
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private lateinit var playerViewModel: PlayerViewModel
@@ -19,6 +24,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        updatePlayerProgress()
         playerViewModel =
             ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory(application)).get(
                 PlayerViewModel::class.java
@@ -28,16 +34,68 @@ class MainActivity : AppCompatActivity() {
                 })
 
                 videoResolution.observe(this@MainActivity, Observer {
+                    seekBar.max = mediaPlayer.duration
                     playerFrame.post {
                         resizePlayer(it.first, it.second)
                     }
                 })
 
+                controllerFrameVisibility.observe(this@MainActivity, Observer {
+                    controllerFrame.visibility = it
+                })
+
+                bufferPercent.observe(this@MainActivity, Observer {
+                    seekBar.secondaryProgress = seekBar.max * it / 100
+                })
+
+                playerStatus.observe(this@MainActivity, Observer {
+                    contolBtn.isClickable = true
+
+                    when (it) {
+                        PlayerStatus.Paused -> {
+                            contolBtn.setImageResource(R.drawable.ic_play)
+                        }
+                        PlayerStatus.Completed -> {
+                            contolBtn.setImageResource(R.drawable.ic_replay)
+                        }
+                        PlayerStatus.NotReady -> {
+                            contolBtn.isClickable = false
+                        }
+                        else -> {
+                            contolBtn.setImageResource(R.drawable.ic_pause)
+                        }
+                    }
+                })
             }
         lifecycle.addObserver(playerViewModel.mediaPlayer)
+
+        playerFrame.setOnClickListener {
+            playerViewModel.toggleControllerVisibility()
+        }
+
+        contolBtn.setOnClickListener {
+            playerViewModel.togglePlayerStatus()
+        }
+
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if (fromUser) { // 是否是用户拖动
+                    playerViewModel.playerSeekToProgress(progress)
+
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+        })
+
         surfaceView.holder.addCallback(object : SurfaceHolder.Callback {
-
-
             override fun surfaceChanged(
                 holder: SurfaceHolder?,
                 format: Int,
@@ -67,6 +125,7 @@ class MainActivity : AppCompatActivity() {
 
         if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
             hideSystemUI()
+            playerViewModel.emmitVideoResolution()
         }
     }
 
@@ -82,12 +141,27 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    private fun updatePlayerProgress() {
+        lifecycleScope.launch {
+            while (true) {
+                delay(500)
+                seekBar.progress = playerViewModel.mediaPlayer.currentPosition
+
+            }
+
+
+        }
+
+
+    }
+
+
     // https://developer.android.com/training/system-ui/immersive?hl=zh-cn
     private fun hideSystemUI() {
         // Enables regular immersive mode.
         // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
         // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                 // Set the content to appear under the system bars so that the
                 // content doesn't resize when the system bars hide and show.
                 or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
--- MyPlayer/app/src/main/java/com/example/myplayer/PlayerViewModel.kt
+++ MyPlayer/app/src/main/java/com/example/myplayer/PlayerViewModel.kt
@@ -6,16 +6,34 @@ import android.view.View
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+enum class PlayerStatus {
+    Playing, Paused, Completed, NotReady
+}
 
 class PlayerViewModel : ViewModel() {
 
-     val mediaPlayer = MyMediaPlayer()
+    val mediaPlayer = MyMediaPlayer()
+
+    private var controllerShowTime = 0L
 
     private val _progressBarVisibility = MutableLiveData(View.VISIBLE)
     val progressBarVisibility: LiveData<Int> = _progressBarVisibility
 
+    private val _controllerFrameVisibility = MutableLiveData(View.INVISIBLE)
+    val controllerFrameVisibility: LiveData<Int> = _controllerFrameVisibility
+
+    private val _bufferPercent = MutableLiveData(0)
+    val bufferPercent: LiveData<Int> = _bufferPercent
+
     private val _videoResolution = MutableLiveData(Pair(0, 0))
     val videoResolution: LiveData<Pair<Int, Int>> = _videoResolution
+
+    private val _playerStatus = MutableLiveData<PlayerStatus>(PlayerStatus.NotReady)
+    val playerStatus: LiveData<PlayerStatus> = _playerStatus
 
     init {
         loadVideo()
@@ -24,19 +42,35 @@ class PlayerViewModel : ViewModel() {
     fun loadVideoPath(videoPath: String) {
         mediaPlayer.apply {
 //            reset()
-            _progressBarVisibility.value = View.INVISIBLE
+            _progressBarVisibility.value = View.VISIBLE
+            _playerStatus.value = PlayerStatus.NotReady
 
             setDataSource(videoPath)
             setOnPreparedListener {
                 _progressBarVisibility.value = View.INVISIBLE
-                isLooping = true
+//                isLooping = true
                 it.start()
+                _playerStatus.value = PlayerStatus.Playing
 
             }
 
             setOnVideoSizeChangedListener { mp, width, height ->
                 _videoResolution.value = Pair(width, height)
 
+            }
+
+            setOnBufferingUpdateListener { mp, percent ->
+                _bufferPercent.value = percent
+            }
+
+            setOnCompletionListener {
+                _playerStatus.value = PlayerStatus.Completed
+
+            }
+
+            setOnSeekCompleteListener {
+                mediaPlayer.start()
+                _progressBarVisibility.value = View.INVISIBLE
             }
 
             prepareAsync()
@@ -58,6 +92,54 @@ class PlayerViewModel : ViewModel() {
         super.onCleared()
 
         mediaPlayer.release()
+    }
+
+    fun emmitVideoResolution() {
+        _videoResolution.value = _videoResolution.value
+    }
+
+    fun toggleControllerVisibility() {
+        if (_controllerFrameVisibility.value == View.INVISIBLE) {
+            _controllerFrameVisibility.value = View.VISIBLE
+            controllerShowTime = System.currentTimeMillis()
+
+            // 延迟3秒隐藏
+            viewModelScope.launch {
+                delay(3000)
+                // 防止重复点击，进行处理
+                if (System.currentTimeMillis() - controllerShowTime > 3000) {
+                    _controllerFrameVisibility.value = View.INVISIBLE
+                }
+            }
+        } else {
+            _controllerFrameVisibility.value = View.INVISIBLE
+        }
+    }
+
+    fun togglePlayerStatus() {
+        when(_playerStatus.value) {
+            PlayerStatus.Playing -> {
+                mediaPlayer.pause()
+                _playerStatus.value = PlayerStatus.Paused
+            }
+            PlayerStatus.Paused -> {
+                mediaPlayer.start()
+                _playerStatus.value = PlayerStatus.Playing
+            }
+            PlayerStatus.Completed -> {
+                mediaPlayer.start()
+                _playerStatus.value = PlayerStatus.Playing
+            }
+            else -> return
+
+        }
+
+    }
+
+
+    fun playerSeekToProgress(process: Int) {
+        _progressBarVisibility.value = View.VISIBLE
+        mediaPlayer.seekTo(process)
     }
 
 }
